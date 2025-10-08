1be3147aba13299b5bbd2fcd1408e2b603be8f5a
Denis Saplitsyn <packman17@mail.ru>
2025-10-08 15:43:37 +0300

    add properties for dto

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    implementation("org.mapstruct:mapstruct-processor:1.6.3")
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
