715416fef74dfcf78e02f4de1a5e44de4e3ef11f
Denis Saplitsyn <packman17@mail.ru>
2025-10-09 11:44:51 +0300

    remove unuse import

--- src/main/java/com/example/spring/dto/BookUpdateDTO.java
+++ src/main/java/com/example/spring/dto/BookUpdateDTO.java
@@ -1,6 +1,5 @@
 package com.example.spring.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import org.openapitools.jackson.nullable.JsonNullable;
